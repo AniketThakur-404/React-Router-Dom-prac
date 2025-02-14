f05712f93f5030a0146944d1ebdb30f80c08e633
Aniket Thakur <163874673+AniketThakur-404@users.noreply.github.com>
2025-02-14 15:15:08 +0530

    Add files via upload

--- src/navbar/pages/Contact.jsx
+++ src/navbar/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='pages'>Welcome to Contact page</div>
   )
 }
 
--- src/navbar/pages/Services.jsx
+++ src/navbar/pages/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='pages'> welcome to Services page</div>
   )
 }
 
